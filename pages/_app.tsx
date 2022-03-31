30ad60c08cb841580df63a39f854cb2c13f2b5d8
jakob153 <guenayjakob@gmail.com>
2022-03-31 23:23:10 +0200

    add title tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,7 @@ export default function MyApp(props: MyAppProps) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
+        <title>Jakob Günay | CV</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
