526abaceb2a0aaf1f2ade1b3a8f98bc971006f66
jakob153 <guenayjakob@gmail.com>
2024-02-21 17:25:26 +0100

    change surname

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -38,7 +38,7 @@ const Profile = () => {
         align="center"
         variant="h4"
       >
-        Jakob Günay
+        Jakob Kruk
       </Typography>
       <Typography align="center">Freelancer Fullstack Dev</Typography>
       <Typography
