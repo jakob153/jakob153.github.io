323c430f2c0929143b88fd397c85c473ab3a74a8
jakob153 <guenayjakob@gmail.com>
2024-02-29 18:38:28 +0100

    Updates

---
+++ _next/static/Tbno5fnWzvxGybe4LD3vX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/480-4281c58392e61119.js","static/chunks/pages/index-a62d0110b0dfb7bc.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
